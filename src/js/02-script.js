a9dc46b221f24f8a15d7ca855c23f724abec8744
chelwillians <michel_wrq@hotmail.com>
2024-06-26 16:56:20 -0300

    base

---
+++ src/js/02-script.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', function () {
+    const hamburguerItem = document.querySelector('.main-header__hamburguer');
+    const headerMenus = document.querySelector('.main-header__menus');
+    hamburguerItem.addEventListener('click', (e) => {
+        hamburguerItem.classList.toggle('active');
+        headerMenus.classList.toggle('active');
+    })
+})
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const swiper = new Swiper(".main-banner", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+    });
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const bannerHeight = document.querySelector('.main-banner').offsetHeight;
+    const mainHeader = document.querySelector('.main-header');
+    console.log(bannerHeight);
+    window.addEventListener('scroll', (e) => {
+        if (this.scrollY >= bannerHeight) {
+            mainHeader.classList.add('fixed')
+            mainHeader.classList.remove('offset')
+        } else if (this.scrollY >= (mainHeader.offsetHeight * 3)) {
+            mainHeader.classList.add('offset')
+            mainHeader.classList.remove('fixed')
+        } else {
+            mainHeader.classList.remove('fixed')
+            mainHeader.classList.remove('offset')
+        }
+    })
+});
